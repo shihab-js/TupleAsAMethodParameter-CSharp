a91c726bbb50d61eae416091d3630a6acde23310
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-26 23:14:54 +0600

    Program completed...

--- TupleAsAMethodParameter/Program.cs
+++ TupleAsAMethodParameter/Program.cs
@@ -6,7 +6,19 @@ namespace TupleAsAMethodParameter
     {
         static void Main(string[] args)
         {
-            
+            //Create a tuple
+            var person = Tuple.Create(1, "Shihab", "Programmer");
+
+            //Pass tuple in a method
+            DisplayPerson(person);
+
+        }
+        //create DisplayPerson Method
+        static void DisplayPerson(Tuple<int,string,string> person)
+        {
+            Console.WriteLine($"Id = {person.Item1}");
+            Console.WriteLine($"Name = {person.Item2}");
+            Console.WriteLine($"Profession = {person.Item3}");
         }
     }
 }
